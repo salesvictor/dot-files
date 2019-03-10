93e5a5fb680862cdd5716da96aba3be719b83842
Victor Sales <salesrvictor@gmail.com>
2019-03-09 23:05:41 -0300

    Some things

--- wifi/iec_pavsup.sh
+++ wifi/iec_pavsup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 killall wpa_supplicant
+ip link set wlp1s0 down
 ip link set wlp1s0 up
 wpa_supplicant -B -i wlp1s0 -c<(wpa_passphrase 'IEC_PAVSUP04' 'wifiI&Cp@vSup2016')
 dhcpcd start wlp1s0
